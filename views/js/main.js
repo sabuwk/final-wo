78d15c404ca73facc84b8c85822a937f30d3d3b7
Daniel <daniel.thiel@ymail.com>
2016-09-25 19:15:50 +0200

    das

--- views/js/main.js
+++ views/js/main.js
@@ -497,16 +497,11 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
 // Moves the sliding background pizzas based on scroll position
-
-
-
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-  var left = items[i].basicLeft;
-
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
